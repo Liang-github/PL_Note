1a5fcfdac463581ca38ddc751ddce4518cc37b8a
LIANG <pl941227@icloud.com>
2017-08-16 17:23:05 +0800

    首页

--- PL_Note/ViewController/ForgetPasswordViewController.m
+++ PL_Note/ViewController/ForgetPasswordViewController.m
@@ -7,31 +7,71 @@
 //
 
 #import "ForgetPasswordViewController.h"
+#import "AllUtils.h"
+#import "Constant.h"
+#import <BmobSDK/Bmob.h>
 
+#import <SMS_SDK/SMSSDK.h>
+#import <SMS_SDK/SMSSDKCountryAndAreaCode.h>
+#import <SMS_SDK/SMSSDK+DeprecatedMethods.h>
+#import <SMS_SDK/SMSSDK+ExtexdMethods.h>
+#import <MOBFoundation/MOBFoundation.h>
 @interface ForgetPasswordViewController ()
 
+@property (weak, nonatomic) IBOutlet UITextField *phoneTextField;
+@property (weak, nonatomic) IBOutlet UITextField *validateCodeTextField;
+@property (weak, nonatomic) IBOutlet UITextField *nowPasswordTextField;
 @end
 
 @implementation ForgetPasswordViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+}
+- (IBAction)getValidateCodeButtonPressed:(id)sender {
+    //获取验证码，要先判断手机号是否已经注册，如果没有注册，则不能获取验证码，让用户去注册界面
+    [self isRepeatUsername:USER_TABLE username:self.phoneTextField.text limitCount:50];
+}
+- (IBAction)resettingPasswordButtonPressed:(id)sender {
+}
+- (IBAction)loginButtonPressed:(id)sender {
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark - 查询手机号是否已经注册
+- (void)isRepeatUsername:(NSString *)tableName username:(NSString *)username limitCount:(int)limitCount {
+    __block BOOL isRepeatUsername = NO;
+    BmobQuery *queryUser = [BmobQuery queryWithClassName:tableName];
+    queryUser.limit = limitCount;
+    [queryUser findObjectsInBackgroundWithBlock:^(NSArray *array, NSError *error) {
+        if (!error) {
+            for (BmobObject *obj in array) {
+                if ([[obj objectForKey:@"username"] isEqualToString:username]) {
+                    //表示已经存在该用户名，可以进行找回密码
+                    isRepeatUsername = YES;
+                    break;
+                }
+            }
+        } else {
+            [AllUtils showPromptDialog:@"提示" andMessage:@"网络异常，请稍后重试！" OKButton:@"确定" OKButtonAction:nil cancelButton:@"" cancelButtonAction:nil  contextViewController:self];
+        }
+        if (!isRepeatUsername) {
+            [AllUtils showPromptDialog:@"提示" andMessage:@"用户还未注册，请先注册！" OKButton:@"确定" OKButtonAction:nil cancelButton:@"" cancelButtonAction:nil  contextViewController:self];
+        } else {
+            [SMSSDK getVerificationCodeByMethod:SMSGetCodeMethodSMS phoneNumber:self.phoneTextField.text zone:@"86" customIdentifier:nil result:^(NSError *error) {
+                if (!error) {
+                    [AllUtils showPromptDialog:@"提示" andMessage:@"验证码发送成功，请稍后！" OKButton:@"确定" OKButtonAction:nil cancelButton:@"" cancelButtonAction:nil  contextViewController:self];
+                } else {
+                    [AllUtils showPromptDialog:@"提示" andMessage:@"获取验证码失败！" OKButton:@"确定" OKButtonAction:nil cancelButton:@"" cancelButtonAction:nil  contextViewController:self];
+                }
+            }];
+        }
+    }];
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+#pragma mark - 点击屏幕隐藏软键盘
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    [self.phoneTextField resignFirstResponder];
+    [self.validateCodeTextField resignFirstResponder];
+    [self.nowPasswordTextField resignFirstResponder];
 }
-*/
-
 @end
